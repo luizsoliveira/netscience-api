5fe49b2c0e561a506ba33a3b0c908685d60d6fd6
Luiz Oliveira <luizsoliveira@gmail.com>
2023-10-05 16:04:14 -0700

    Creating a task example

--- sql/011_mock_data.sql
+++ sql/011_mock_data.sql
@@ -10,7 +10,15 @@ INSERT INTO public.users(
 	first_name, last_name, email)
 	VALUES ('Luiz', 'Oliveira', 'luizsoliveira@gmail.com');
 
-
 INSERT INTO public.projects(
 	title, manager_user_id)
 	VALUES ('CyberDefense', 1);
+
+INSERT INTO public.tasks(
+	project_id, title, description, task_type_id, parameters)
+	VALUES (1,
+			'Event 1',
+			'',
+			1,
+			'{"collection_site":"ripe","date_start":"20050523","time_start":"0000","date_end":"20050527","time_end":"2359","anomalous_date_start":"20050525","anomalous_time_start":"0400","anomalous_date_end":"20050525","anomalous_time_end":"0759","data_partition_training":60,"data_partition_testing":30,"rnn_length":"10","cache":"activated","debug":"activated"}'
+			);
